6f6d132ce7ac3487851a0a922d25d3113530a293
Raghav Mohan <raghav.mohan@gmail.com>
2013-02-08 15:16:23 -0600

    testing facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '131364997031342', '44c516fb668a71852347686ec508cbef'
 end
